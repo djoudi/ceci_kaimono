11b7633d987b06d6acf1e5bacf8af62398bacbab
Shin New Dell Win <okada.shin@gmail.com>
2011-05-20 15:04:27 +0200

    kaimonokago updates for hiding other languages when Setting is not set with multi-language

--- system/application/views/public/header.php
+++ system/application/views/public/header.php
@@ -10,6 +10,43 @@
 
 <body>
 <div id="wrapper">
+    <div id="topnav">
+       <?php
+if(count($this->data['mainnav'])){
+    echo "\n<ul id='nav'>";
+
+    foreach ($this->data['mainnav'] as $key => $menu){
+         if($menu['lang_id']==$this->data['lang_id']){
+        echo "\n<li class='menuone'>\n";
+        // need to find page uri from id
+        // menu /page_uri/lang_id
+       
+        echo anchor ($this->data['mainmodule']."/pages/".$menu['id'], $menu['name']);
+            if (count($menu['children'])){
+                echo "\n<ul>";
+                    foreach ($menu['children'] as $subkey => $submenu){
+                    echo "\n<li class='menutwo'>\n";
+                    echo anchor($this->data['mainmodule']."/pages/".$submenu['id'],$submenu['name']);
+                        if (count($submenu['children'])){
+                        echo "\n<ul>";
+                        foreach ($submenu['children'] as $subkey => $subsubname){
+                        echo "\n<li class='menuthree'>\n";
+                        echo anchor($this->data['mainmodule']."/cat/",$subsubname['name']);
+                        echo "\n</li>";
+                        }
+                        echo "\n</ul>";
+                        }
+                    echo "\n</li>";
+                    }
+                echo "\n</ul>";
+            }
+            echo "\n</li>\n";
+    }
+    }
+echo "\n</ul>\n";
+}
+?>
+    </div>
     <div id="header">
         <h1><?php print $this->preference->item('site_name')?></h1>
     </div>
